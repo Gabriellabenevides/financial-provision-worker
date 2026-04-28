0572b33a046be6365ca8b94868621f1d5c64788d
gabi <gabriellabenevides22@gmail.com>
2026-04-28 18:44:02 -0300

    Refatoração e remoção de projetos e configurações
    Este commit realiza uma reestruturação significativa no projeto `Financial-Provision.Worker`, incluindo a remoção de vários arquivos e configurações.
    
    - O arquivo de solução foi atualizado, removendo projetos, incluindo `Financial-Provision.Worker.Application`.
    - O arquivo de projeto `Financial-Provision.Worker.csproj` teve suas referências de pacotes e configurações de framework eliminadas.
    - A configuração de endereço do host foi removida do arquivo `Financial-Provision.Worker.http`.
    - O arquivo `Program.cs` foi simplificado, com a remoção de instruções `using` e a lógica de configuração do `DbContext`.
    - O arquivo `launchSettings.json` foi completamente removido.
    - A implementação do worker em `DevolucaoAprovadaWorker.cs` e do consumidor em `DevolucaoAprovadaConsumer.cs` foi eliminada.
    - As configurações de logging foram removidas de `appsettings.Development.json`, e a configuração do RabbitMQ foi retirada de `appsettings.json`.
    - A definição da classe `DevolucaoAprovadaEvent` e a interface `IMessageHandler` foram removidas, impactando a lógica de eventos e tratamento de mensagens.
    - O arquivo `FinancialProvision.Worker.Application.csproj` e `FinancialProvision.Worker.Infraestructure.csproj` também tiveram suas configurações e referências removidas.
    
    Essas mudanças podem afetar a lógica de negócios e a comunicação com serviços de mensageria.

--- Financial-Provision.Worker/FinancialProvision.Worker.Application/Events/DevolucaoAprovadaEvent.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FinancialProvision.Worker.Application.Events;
-
-public class DevolucaoAprovadaEvent
-{
-    public int DevolucaoId { get; set; }
-    public decimal Valor { get; set; }
-    public int Ano { get; set; }
-    public int Mes { get; set; }
-}
--- Financial-Provision.Worker/FinancialProvision.Worker.Application/Interfaces/IMessageHandler.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace FinancialProvision.Worker.Application.Interfaces;
-
-public interface IMessageHandler<T>
-{
-    Task Handle(T message);
-}
